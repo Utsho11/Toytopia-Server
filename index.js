2a02762a31b902a3d539eb80d75ee91d49301b02
Utsho11 <utshoroy521@gmail.com>
2023-05-20 15:10:34 +0600

    update api created

--- index.js
+++ index.js
@@ -24,16 +24,16 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
+
     const toysCollections = client.db('toy-shop-DB').collection('All-toys')
-    
-    app.get('/allToys', async (req,res) =>{
+
+    app.get('/allToys', async (req, res) => {
       let query = {};
-      if(req.query?.email){
-        query={email: req.query.email}
+      if (req.query?.email) {
+        query = { email: req.query.email }
       }
       const result = await toysCollections.find(query).toArray();
       res.send(result);
@@ -43,20 +43,52 @@ async function run() {
     //   res.send(result);
     // })
 
-    app.get('/allToys/:id', async (req,res) =>{
+    app.get('/allToys/:id', async (req, res) => {
       const id = req.params.id;
       console.log(id);
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await toysCollections.findOne(query);
       res.send(result);
       console.log(result);
     })
 
-    app.post('/allToys', async (req,res) =>{
+    app.put('/allToys/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedToy = req.body;
+      const newUpdatedToy = {
+        $set: {
+          picture: updatedToy.picture,
+          price: updatedToy.price,
+          seller: updatedToy.seller,
+          toy_name: updatedToy.toy_name,
+          quantity: updatedToy.quantity,
+          sub_category: updatedToy.sub_category,
+          description: updatedToy.description,
+          rating: updatedToy.rating,
+          email: updatedToy.email
+        }
+      }
+      const result = await toysCollections.updateOne(filter, newUpdatedToy, options);
+      res.send(result);
+    })
+
+    app.post('/allToys', async (req, res) => {
       const addedToy = req.body;
       console.log(addedToy);
       const result = await toysCollections.insertOne(addedToy);
       res.send(result);
+    })
+    app.delete('/allToys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollections.deleteOne(query);
+      res.send(result);
+    })
+    app.put('/allToys/:id', async (req, res) => {
+      const updatedMyToy = req.body;
+
     })
 
 
@@ -73,10 +105,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/',(req,res) =>{
-    res.send('toy server is running');
+app.get('/', (req, res) => {
+  res.send('toy server is running');
 })
 
-app.listen(port,() => {
-    console.log(`toy server is running on : ${port}`);
+app.listen(port, () => {
+  console.log(`toy server is running on : ${port}`);
 })
