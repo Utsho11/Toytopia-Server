56c010e8236e477a267d62353f9f5077eb0b9e18
Utsho11 <utshoroy521@gmail.com>
2023-05-21 09:00:55 +0600

    vercel issue fixed

--- index.js
+++ index.js
@@ -19,14 +19,22 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  maxPoolSize: 10,
 });
 
 async function run() {
   try {
 
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   client.connect((error) => {
+    if(error){
+      console.error(error);
+      return;
+    }
+   });
 
     const toysCollections = client.db('toy-shop-DB').collection('All-toys')
 
